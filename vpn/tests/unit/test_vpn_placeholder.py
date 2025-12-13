fb2b62ac4b7cb263cb1aa03bc12ac0b66212d187
Adhitya Ravi <adhitya.ravi@canonical.com>
2025-12-13 03:59:22 +0100

    feat[vpn]: adds vpn gateway interfaces classes

--- vpn/tests/unit/test_vpn_placeholder.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2025 The Charmarr Project
-# See LICENSE file for licensing details.
-
-"""Placeholder test to validate test infrastructure for vpn package."""
-
-
-def test_vpn_placeholder() -> None:
-    """Placeholder test that always passes."""
-    assert True
